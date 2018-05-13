32b3c3abacc4eb5d92d4c245fbdf45bc6bc047c5
dracit7 <zglzhz@163.com>
2018-05-13 13:57:10 +0800

    Added TCP method and local cache
        In order to make users' experience better, an optional choice:
    TCP method has been added, and the server now supports local cache.

--- DNS_server.py
+++ DNS_server.py
@@ -6,24 +6,41 @@ import socket
 import struct
 import threading
 import queue
+import redis
 
 import lib_Parsers
 from lib_Parsers import RequestParser
 from lib_Parsers import AnswerParser
 
 MAX_SIZE=4096
+MAX_CLIENT=5
+DATABASE_NAME='DNS_server_cache'
 AnswerQueue=queue.Queue()
 
 class SendRequest(threading.Thread):
   def __init__(self,post,servaddr,cliaddr):
     threading.Thread.__init__(self)
+    self.cache=redis.Redis(host='127.0.0.1',port=6379,db=0)
     self.Sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     self.PostContext=post
     self.Servaddress=servaddr
-    self.Clientaddrss=cliaddr
+    self.Clientaddress=cliaddr
   def run(self):
-    self.Sock.sendto(self.PostContext,self.Servaddress)
-    AnswerQueue.put((self.Sock.recv(MAX_SIZE),self.Clientaddrss))
+    url=RequestParser(self.PostContext)['url']
+    QuestionNum=RequestParser(self.PostContext)['QuestionNum']
+    cached=True
+    for i in range(0,QuestionNum):
+      if self.cache.hexists(DATABASE_NAME,url[i]) == 0:
+        cached=False
+    if cached == True:
+      for i in range(0,QuestionNum):
+        AnswerQueue.put((self.cache.hget(DATABASE_NAME,url[i]),self.Clientaddress))
+    else:
+      self.Sock.sendto(self.PostContext,self.Servaddress)
+      for i in range(0,QuestionNum):
+        Answer=self.Sock.recv(MAX_SIZE)
+        self.cache.hset(DATABASE_NAME,url[i],Answer)
+        AnswerQueue.put((Answer,self.Clientaddress))
 
 class SendAnswer(threading.Thread):
   def __init__(self):
@@ -36,9 +53,22 @@ class SendAnswer(threading.Thread):
         self.Sock.sendto(Post,Cliaddr)
         print('Sent answer to ',Cliaddr)
 
+class TCPconnect(threading.Thread):
+  def __init__(self,servaddr,Clisock):
+    threading.Thread.__init__(self)
+    self.Serversock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    self.Clientsock=Clisock
+    self.servaddr=servaddr
+  def run(self):
+    Post=self.Clientsock.recv(MAX_SIZE)
+    self.Serversock.sendto(Post,self.servaddr)
+    Post=self.Serversock.recv(MAX_SIZE)
+    self.Clientsock.send(Post)
+    self.Clientsock.close()
+    print('Connection closed.')
 
 class DNS_Server:
-  def __init__(self,mode='UDP',server="8.8.8.8",port=9290):
+  def __init__(self,mode='UDP',server="8.8.8.8",port=9190):
     self.mode=mode
     if mode=='UDP':
       self.Socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
@@ -47,8 +77,11 @@ class DNS_Server:
     else:
       print('Error : Invalid protocol.\nPlease check your params.')
       sys.exit(1)
-    self.Socket.bind(('127.0.0.1',port))
+    if self.Socket.bind(('127.0.0.1',port)) == -1:
+      print('Error : Bind error. Ensure that this port is not used.')
+      sys.exit(1)
     self.serverIP=server
+    print('The DNS server is running now.')
   def start(self):
     Server_address=(self.serverIP,53)
     if self.mode=='UDP':
@@ -59,11 +92,21 @@ class DNS_Server:
         print('Get DNS request from ',Cliaddr)
         Request_thread=SendRequest(Post,Server_address,Cliaddr)
         Request_thread.start()
+    if self.mode=='TCP':
+      self.Socket.listen(MAX_CLIENT)
+      while True:
+        Clisock,Cliaddr=self.Socket.accept()
+        print('Connected with ',Cliaddr)
+        thread=TCPconnect(Server_address,Clisock)
+        thread.start()
+      
+
         
 
 
 
 if __name__ == "__main__":
-  server=DNS_Server('UDP')
+  protocol=input("Please enter the protocol you want to use : ")
+  server=DNS_Server(protocol)
   server.start()
   
--- README.md
+++ README.md
@@ -6,14 +6,27 @@
 ***
 ## Features
 
-1. Only support UDP yet
+1. Supports TCP and UDP methods
 2. It's multi-threaded with a message Queue
-3. No local cache yet
+3. Use redis as its database for local cache
 4. It's default DNS server is '8.8.8.8'
 
 ## Usage
 
+    As this script uses Redis as its cache stroing system ,
+    you're required to install Redis to run this program.
+    
+    Install Redis : pacman -S redis (take arch as an example)
+    Install redis.py : check `https://pypi.org/project/redis/`
+
 	`$python DNS_server.py`
+	
+## Updated
+
+1. Added TCP method
+2. Added local cache, but as the AnswerBuilder is not fully developed,
+local cache stores AnswerPost instead of IP address.
+3. Fixed bug : multi-question post should be answered twice
 
 
 
